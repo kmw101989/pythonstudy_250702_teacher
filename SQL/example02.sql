0baeef5727c1e94c60b188bb6d33fddc6d4caede
Divjason <sejinpark@tcocs.com>
2025-07-31 18:04:20 +0900

    250731

---
+++ SQL/example02.sql
@@ -0,0 +1,36 @@
+-- CREATE DATABASE IF NOT EXISTS Dave;
+-- USE Dave;
+
+-- CREATE TABLE mytable (
+-- 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+--     name VARCHAR(50) NOT NULL,
+--     modelnumber VARCHAR(15) NOT NULL,
+--     series VARCHAR(30) NOT NULL,
+--     PRIMARY KEY(id)
+-- );
+
+-- DESC mytable;
+
+-- ALTER TABLE mytable MODIFY COLUMN
+-- name VARCHAR(20) NOT NULL;
+
+-- ALTER TABLE mytable CHANGE COLUMN
+-- modelnumber model_num VARCHAR(10) NOT NULL;
+
+-- ALTER TABLE mytable CHANGE COLUMN
+-- series model_type VARCHAR(10) NOT NULL;
+
+# CREATE DATABASE IF NOT EXISTS dave;
+# USE dave;
+# CREATE TABLE model_info ()
+
+-- DROP TABLE IF EXISTS model_info;
+-- CREATE TABLE model_info (
+-- 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+--     name VARCHAR(20) NOT NULL,
+--     model_num VARCHAR(10) NOT NULL,
+--     model_type VARCHAR(10) NOT NULL,
+--     PRIMARY KEY(id)
+-- );
+
+DESC model_info;
---
+++ SQL/example03.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE IF NOT EXISTS school;
+USE school;
+
+-- CREATE TABLE students (
+-- 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+--     PRIMARY KEY(id)
+-- );
+
+CREATE TABLE students (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    age INT UNSIGNED,
+    grade VARCHAR(10)
+);
+
+DESC students;
+
+-- INSERT INTO students VALUES(1, "강백호", 15, "8");
+
+INSERT INTO students (name, age, grade)
+VALUES("서태웅", 15, "8");
+
+INSERT INTO students (grade, name, age)
+VALUES("10", "채치수", 17);
+
+SELECT * FROM students;
+
+INSERT INTO students (grade, name, age)
+VALUES("9", "정대만", 16);
+
+INSERT INTO students (grade, name, age)
+VALUES("8", "송태섭", 15);
+
+INSERT INTO students (grade, name, age)
+VALUES
+	("10", "mmm", 15),
+	("11", "das", 15),
+    ("12", "kof", 16)
+;
+
+SELECT * FROM students;
+SELECT name FROM students;
+SELECT grade FROM students;
+SELECT name, grade FROM students;
+
+SELECT * FROM students WHERE age = 16; # 대입연산자
+SELECT * FROM students WHERE age != 15; # 부정연산자 - 1
+SELECT * FROM students WHERE age <> 15; # 부정연산자 - 2
+
